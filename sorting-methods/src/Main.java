0879979bcb954a3b021d167203d5b4a9159dd6d4
Guilherme Souza <guilhermesouza281@protonmail.com>
2024-03-15 22:12:30 -0300

    Revert "feat: quick sort without pivot finished + getPosAtList function"
    This reverts commit e14996c4a9455cb70777ad984b7cd9d47e12b902.

--- sorting-methods/src/Main.java
+++ sorting-methods/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
             list.insertEnd(randomValue);
         }
         list.display();
-        list.quickSortWithoutPivot();
+        list.insertionSort();
         list.display();
     }
 }
